bece72da6ab9b83de18b2c4f511f9735f3a7a311
John Colagioia <john@colagioia.net>
2014-06-24 17:28:11 -0400

    Quick refactor for easier asynchronous activity.

--- NonRazorWeb/Controllers/HomeController.cs
+++ NonRazorWeb/Controllers/HomeController.cs
@@ -18,6 +18,16 @@ namespace NonRazorWeb.Controllers
         public class HomeController : Controller
         {
                 /// <summary>
+                /// The running HTML report.
+                /// </summary>
+                private string html = string.Empty;
+
+                /// <summary>
+                /// The game is done.
+                /// </summary>
+                private bool done = false;
+
+                /// <summary>
                 /// Controller for the front page.
                 /// </summary>
                 /// <returns>The view.</returns>
@@ -25,21 +35,44 @@ namespace NonRazorWeb.Controllers
                 {
                         Game g = new Game();
                         string s = string.Empty;
-                        string html = string.Empty;
 
-                        while (g.Done())
+                        while (!done)
                         {
-                                s = g.TakeTurn();
+                                s = ShowNextPlay(g);
                                 if (!string.IsNullOrWhiteSpace(s))
                                 {
-                                        html += s.Replace(Environment.NewLine, "<br>") + "<br>\n";
+                                        this.html += s.Replace(Environment.NewLine, "<br>") + "<br>\n";
                                 }
                         }
 
-                        html += g.FinalTally();
-
-                        this.ViewData["Message"] = html;
+                        this.ViewData["Message"] = this.html;
                         return this.View();
+                }
+
+                /// <summary>
+                /// Shows the next play.
+                /// </summary>
+                /// <returns>The next play.</returns>
+                /// <param name="g">The game component.</param>
+                private string ShowNextPlay(Game g)
+                {
+                        string s = string.Empty;
+
+                        if (done)
+                        {
+                                // Do nothing
+                        }
+                        else if (g.Done())
+                        {
+                                s = g.FinalTally();
+                                done = true;
+                        }
+                        else
+                        {
+                                s = g.TakeTurn();
+                        }
+
+                        return s;
                 }
         }
 }
